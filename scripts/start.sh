841791ccee282c0ed4689fedaa750fb7d3d99b26
Mark1237200 <0915kimjw@gmail.com>
2022-08-09 15:40:42 +0900

    states

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # 아래 위치는 여러분이 해당 리포지토리를 클론한 위치로 설정해주세요.
-cd C:\Users\0915k\OneDrive\바탕 화면\WEB FE\sprint/fe-sprint-practice-deploy
+cd C:\Users\0915k\OneDrive\"바탕 화면"\"WEB FE"\sprint/fe-sprint-practice-deploy
 authbind --deep pm2 start app.js
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # 아래 위치는 여러분이 해당 리포지토리를 클론한 위치로 설정해주세요.
-cd C:\Users\0915k\OneDrive\바탕 화면\WEB FE\sprint/fe-sprint-practice-deploy
+cd C:\Users\0915k\OneDrive\"바탕 화면"\"WEB FE"\sprint/fe-sprint-practice-deploy
 pm2 stop app.js 2> /dev/null || true
 pm2 delete app.js 2> /dev/null || true
